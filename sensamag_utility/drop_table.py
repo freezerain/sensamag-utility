3d4d58585088c7cf17e204664ca32c1d5d22c2f7
Freeze <rain-ilia@rambler.ru>
2022-09-27 16:10:49 +0200

    drop table command

---
+++ sensamag_utility/drop_table.py
@@ -0,0 +1,12 @@
+import mariadb
+from rich import print
+
+def drop_text_table(conn: mariadb.Connection):
+    print("> Removing rows from textcontents and textreferences tables!")
+    cur = conn.cursor()
+    try: 
+        cur.execute("DROP TABLE textcontents; DROP TABLE textreferences;")
+        cur.commit()
+        print(f"> [bold green]Removed successfully [yellow]{cur.affected_rows}[/yellow] rows.")
+    except mariadb.Error as exception:
+        print(f"> [bold red] Error dropping tables:[/] {exception}")
