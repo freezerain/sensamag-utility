f48a9a52161350fd46bfdba40760c9d30299bd02
Freeze <rain-ilia@rambler.ru>
2022-09-27 16:28:36 +0200

    typo fix

--- sensamag_utility/drop_table.py
+++ sensamag_utility/drop_table.py
@@ -4,9 +4,9 @@ from rich import print
 def drop_text_table(conn: mariadb.Connection):
     print("> Removing rows from textcontents and textreferences tables!")
     cur = conn.cursor()
-    try: 
+    try:
         cur.execute("DROP TABLE textcontents; DROP TABLE textreferences;")
-        cur.commit()
+        conn.commit()
         print(f"> [bold green]Removed successfully [yellow]{cur.affected_rows}[/yellow] rows.")
     except mariadb.Error as exception:
         print(f"> [bold red] Error dropping tables:[/] {exception}")
